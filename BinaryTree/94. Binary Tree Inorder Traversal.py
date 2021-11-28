6b538d33f6f48ee6d7e16bcc9f578199c778f2d0
Shivang Gautam <shivanggautam2002friday@gmail.com>
2021-11-28 17:21:33 +0530

    Added Easy: 94

---
+++ BinaryTree/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,28 @@
+"""
+94. Binary Tree Inorder Traversal
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Approach : Just do the inorder traversal and return
+
+"""
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return
+        def inorder(root):
+            if not root:
+                return []
+            return inorder(root.left) + [root.val] + inorder(root.right)
+        return inorder(root)
