5d1de7e18580e0658f95904b76bc18e2106f2bfa
Shivang Gautam <shivanggautam2002friday@gmail.com>
2021-11-28 20:47:50 +0530

    Added Easy: 783 (530-BST)

---
+++ BinaryTree/783. Minimum Distance Between BST Nodes.py
@@ -0,0 +1,38 @@
+"""
+783. Minimum Distance Between BST Nodes
+Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
+Constraints:
+A) The number of nodes in the tree is in the range [2, 10**4].
+B) 0 <= Node.val <= 10**5
+
+Follow up: Same as 530. Minimum absolute differnce in BST.py
+
+Approach:
+Do Inorder Traversal : which is sorted of BST : Run a loop and find min consecutive difference and return the min result
+
+"""
+
+# Definition for a binary tree node.
+from typing import Optional
+import math
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return []
+            return dfs(node.left) + [node.val] + dfs(node.right)
+        ans = dfs(root)
+        res = math.inf
+        for i in range(len(ans)-1):
+            if abs(ans[i]-ans[i+1]) <= res:
+                res = abs(ans[i]-ans[i+1])
+        return res
