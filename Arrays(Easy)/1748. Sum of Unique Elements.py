99a4e9fdad000403f11a7ce2053dfe4f63c7ade8
Shivang Gautam <shivanggautam2002friday@gmail.com>
2021-11-30 17:50:15 +0530

    Added Easy: 1748

---
+++ Arrays(Easy)/1748. Sum of Unique Elements.py
@@ -0,0 +1,27 @@
+"""
+1748. Sum of Unique Elements
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+Constraints:
+A) 1 <= nums.length <= 100
+B) 1 <= nums[i] <= 100
+
+Approach: Create a hashtable and add keys for which value is 1
+
+"""
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        h = {}
+        for each in nums:
+            if each not in h:
+                h[each] = 1
+            else:
+                h[each] += 1
+        ans = 0
+        for k,v in h.items():
+            if v==1:
+                ans += k
+        return ans
