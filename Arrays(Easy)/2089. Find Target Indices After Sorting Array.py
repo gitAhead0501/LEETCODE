87859f8b0dc96293ede50e81dc024259770b94df
Shivang Gautam <shivanggautam2002friday@gmail.com>
2021-12-01 10:03:24 +0530

    Added Easy: 2089

---
+++ Arrays(Easy)/2089. Find Target Indices After Sorting Array.py
@@ -0,0 +1,24 @@
+"""
+2089. Find Target Indices After Sorting Array
+
+You are given a 0-indexed integer array nums and a target element target.
+
+A target index is an index i such that nums[i] == target.
+
+Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
+
+Constraints:
+A) 1 <= nums.length <= 100
+B) 1 <= nums[i], target <= 100
+
+Approach: Just follow the problem statement
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        return [i for i in range(len(nums)) if nums[i]==target]
