8e3d70868e566ee2a2cf13a7e0ed1557d9749528
Shivang Gautam <shivanggautam2002friday@gmail.com>
2021-12-04 16:48:24 +0530

    Added Easy: 1629

---
+++ Arrays(Easy)/1629. Slowest Key.py
@@ -0,0 +1,42 @@
+"""
+1629. Slowest Key
+
+A newly designed keypad was tested, where a tester pressed a sequence of n keys, one at a time.
+
+You are given a string keysPressed of length n, where keysPressed[i] was the ith key pressed in the testing sequence, and a sorted list releaseTimes, where releaseTimes[i] was the time the ith key was released. Both arrays are 0-indexed. The 0th key was pressed at the time 0, and every subsequent key was pressed at the exact time the previous key was released.
+
+The tester wants to know the key of the keypress that had the longest duration. The ith keypress had a duration of releaseTimes[i] - releaseTimes[i - 1], and the 0th keypress had a duration of releaseTimes[0].
+
+Note that the same key could have been pressed multiple times during the test, and these multiple presses of the same key may not have had the same duration.
+
+Return the key of the keypress that had the longest duration. If there are multiple such keypresses, return the lexicographically largest key of the keypresses.
+
+
+Constraints:
+A) releaseTimes.length == n
+B) keysPressed.length == n
+C) 2 <= n <= 1000
+D) 1 <= releaseTimes[i] <= 109
+E) releaseTimes[i] < releaseTimes[i+1]
+F) keysPressed contains only lowercase English letters.
+
+Approach: Create a hashtable for storing the releastime and its value as the key pressed : return the max of h and again return its max
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        h = {}
+        a,i = 0,0
+        while i < len(releaseTimes):
+            if releaseTimes[i]-a not in h:
+                h[releaseTimes[i]-a] = [keysPressed[i]]
+            else:
+                h[releaseTimes[i]-a].append(keysPressed[i])
+            a = releaseTimes[i]
+            i += 1
+        a = max(h.keys())
+        return max(h[a])
