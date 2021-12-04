9ac78ee63534c76ff8b243211a8f20a35c38aa58
Shivang Gautam <shivanggautam2002friday@gmail.com>
2021-12-04 15:16:28 +0530

    Added Easy: 1437

--- Arrays(Easy)/1437. Check If All 1's Are at Least Length K Places Away.py
+++ Arrays(Easy)/1437. Check If All 1's Are at Least Length K Places Away.py
@@ -8,16 +8,30 @@ B) 0 <= k <= nums.length
 C) nums[i] is 0 or 1
 
 Approach: Record the indices of 1's and check if their difference is less than k or not
+# BIT MANIPULATION
 
 """
-
+import math
 from typing import List
 
-
+# 1) TC : O(n) , SC : O(n)
 class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
         last = [i for i in range(len(nums)) if nums[i]]
         for i in range(len(last)-1):
             if last[i+1]-last[i]<k+1:
                 return False
+        return True
+
+
+
+# 1) TC : O(n) , SC : O(1)
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        j = -math.inf
+        for i in range(len(nums)):
+            if nums[i]:
+                if i-j-1<k:
+                    return False
+                j=i
         return True
