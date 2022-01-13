31d6c77e48b6477efb80d0e892d60a8d6c5eb9ee
Shivang Gautam <shivanggautam2002friday@gmail.com>
2022-01-13 22:18:59 +0530

    Added Easy: 1710

---
+++ Greedy(Easy)/1710. Maximum Units on a Truck.py
@@ -0,0 +1,34 @@
+"""
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+
+Constraints:
+A) 1 <= boxTypes.length <= 1000
+B) 1 <= numberOfBoxesi, numberOfUnitsPerBoxi <= 1000
+C) 1 <= truckSize <= 10^6
+
+Approach: Sort the array in descending order and count till valid trucksize
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes.sort(key = lambda x:-x[1])
+        print(boxTypes)
+        res = 0
+        for x,y in boxTypes:
+            if x < truckSize:
+                truckSize -= x
+                res += x*y
+            elif x >= truckSize:
+                res += truckSize*y
+                break
+        return res
