45e9077ab27127ca530637584e369df7f6b473d2
Shivang Gautam <shivanggautam2002friday@gmail.com>
2022-01-14 14:22:34 +0530

    Added Easy: 976

--- Greedy(Easy)/976. Largest Perimeter Triangle.py
+++ Greedy(Easy)/976. Largest Perimeter Triangle.py
@@ -16,8 +16,8 @@ from typing import List
 
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        nums.sort(reverse=True)
-        for i in range(len(nums)-3):
-            if nums[i] > nums[i+1] + nums[i+2]:
+        nums.sort()
+        for i in range(len(nums)-3,-1,-1):
+            if nums[i] + nums[i+1] > nums[i+2]:
                 return nums[i] + nums[i+1] + nums[i+2]
         return 0
